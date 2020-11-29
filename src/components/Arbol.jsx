23ec392469ebdace132b2147a480de5812384657
José L. Gutiérrez <gutiecuador@gmail.com>
2020-11-29 00:03:50 -0500

    update arbol to hooks

--- src/components/Arbol.jsx
+++ src/components/Arbol.jsx
@@ -1,44 +1,45 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import BinaryTree from "family-binary-tree";
 
-class Arbol extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-    };
-  }
+const Arbol = () => {
+  
+  const [usuarios, setUsuarios] = useState([]);
 
-  componentDidMount() {
-    fetch('http://localhost/multinivel/index.php?action=getArbol&id=1')
-      .then(response => response.json())
-      .then(data => this.setState({ data }));
+  const consultarAPI = () => {
+    fetch(`http://localhost/multinivel/index.php?action=getArbol&id=1`)
+    .then(response => response.json())
+    .then(data => setUsuarios( data ));
   }
+  
+  useEffect( () => {
+    consultarAPI();
+  },[]); 
 
-  render() {
-    return(
-      <Fragment>
-          { this.state.data.length > 0 &&
+  return(
+    <Fragment>
+          { usuarios.length > 0 &&
             <BinaryTree
-              allUsers={this.state.data}
-              rootUser={this.state.data[0]}
-              bgSideBar={'#333'}
-              colorText={'#333'}
-              colorSideBar={'#fff'}
-              nameFake={'Sin Usuario'}  
-              bgButton={'blue'}
-              colorButton={'black'}
-              disableNavigation
-              disableSideBar
-              maxDeep={4}
-              /* renderDetail={user => {
-              return null;
-              }} */
-          />
+                allUsers={usuarios}
+                rootUser={usuarios[0]}
+                bgSideBar={'#333'}
+                colorText={'#333'}
+                colorSideBar={'#fff'}
+                nameFake={'Sin Usuario'}  
+                bgButton={'blue'}
+                colorButton={'black'}
+                disableNavigation
+                disableSideBar
+                maxDeep={4}
+                /* renderDetail={user => {
+                return null;
+                }} */
+            />
           }
+
+     
           
-      </Fragment>)
-  }
+    </Fragment>)
+    
 }
 
 export default Arbol;
